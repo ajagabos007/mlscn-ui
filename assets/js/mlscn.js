cd02d289f03d8b7b43a5680e1fd758e3cc37e41d
Philip James Ajagabos <ajagabos007@gmail.com>
2024-09-29 09:25:25 +0100

    update top navbar

--- assets/js/mlscn.js
+++ assets/js/mlscn.js
@@ -10,6 +10,24 @@ $(document).ready(function(){
         $( "#navbarTooggleIcon" ).show();
         $('#navbarBrandMiniText').addClass('d-none');
         $('#navbarBrandMiniText').hide();
-
+        $('.mlscn-dropdown-toggle').closest('li').siblings().find('ul.dropdown-menu').slideUp(); // Hide siblings' dropdowns
+        $('.mlscn-dropdown-toggle').closest('li').siblings().find('a.mlscn-dropdown-toggle').attr('aria-expanded', 'false'); // Collapse aria-expanded
     })
+
+    $('.mlscn-dropdown-toggle').on('click', function(e){
+        e.preventDefault();
+        $(this).closest('li').siblings().find('ul.dropdown-menu').slideUp(); // Hide siblings' dropdowns
+        $(this).closest('li').siblings().find('a.mlscn-dropdown-toggle').attr('aria-expanded', 'false'); // Collapse aria-expanded for other dropdowns
+
+        const currentDropdown = $(this).next('ul.dropdown-menu');
+        const isOpen = currentDropdown.is(':visible');
+
+        $(this).attr('aria-expanded', !isOpen);
+
+        $(this).closest('ul.dropdown-menu').find('ul.dropdown-menu').slideUp();
+
+        currentDropdown.slideToggle();
+       
+    })
+
 });
